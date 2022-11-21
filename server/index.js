c4062dbaa0f88544379458d0adda56d13b420918
Ivan Day <ivanday@utexas.edu>
2022-11-21 17:57:45 -0600

    Add axios package, implement framework of get request to github api

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
@@ -12,7 +13,11 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
-  console.log(req.body);
+  //console.log(req.body);
+  axios.get(`https://api.github.com/users/${req.body.data}/repos`)
+    .then((response) => {
+      console.log(response.data);
+    })
 });
 
 app.get('/repos', function (req, res) {
